17eb17d1bf47a353713498fd1902447c4e65c192
Mathias Lindblom <mathias.lindblom@trustly.com>
2014-05-01 15:08:19 +0200

    finished

--- countZeros/main.cpp
+++ countZeros/main.cpp
@@ -4,30 +4,77 @@
 #include <math.h>
 
 
-std::vector<long> getLongestSequence(long n) {
-    std::vector<long> sequence = {1};
+long countZeros(long m, long n, int index, long zeroCount) {
     
-    for(int val = 2; val <= n; val *= 2) {
-        sequence.push_back(val);
+ //    om pos(10, index) är större än m, räkna upp till första ettan dyker fram
+    long powTenIndex = 1;
+    for(int i = 0; i < index; i++) {
+        powTenIndex *= 10;
     }
-    return sequence;
+    
+    long powTenIndexPlusOne = powTenIndex*10;
+    long diff = n - m;
+    long start = m;
+    if(powTenIndex > m && powTenIndex != 1) {
+        long newDiff = diff - powTenIndex;
+        start = m + diff - newDiff;
+        diff = newDiff;
+    }
+    
+    if(diff < 0) {
+        return zeroCount;
+    }
+    
+    int digit = (start / powTenIndex) % 10;
+    
+    long completeSwitches = diff / powTenIndexPlusOne;
+    long totalSwitches = completeSwitches * powTenIndex;
+    
+    long restAfterCompleteSwitches = diff % powTenIndexPlusOne;
+    
+    int finalDigit = ((start+restAfterCompleteSwitches) / powTenIndex) % 10;
+    
+    if(digit == 0 && finalDigit == 0) {
+        long toAdd;
+        if(restAfterCompleteSwitches <= powTenIndex) {
+            toAdd = restAfterCompleteSwitches + 1;
+        } else {
+            toAdd = powTenIndex - (powTenIndexPlusOne - restAfterCompleteSwitches) + 1;
+        }
+        totalSwitches += toAdd;
+    } else if(digit == 0){
+        long filteredVal = start % powTenIndex;
+        long toAdd = powTenIndex - filteredVal;
+        totalSwitches += toAdd;
+    } else if(finalDigit == 0) {
+        long finalVal = start+restAfterCompleteSwitches;
+        long filteredVal = finalVal % powTenIndex;
+        long toAdd = filteredVal + 1;
+        totalSwitches += toAdd;
+    
+    } else if(finalDigit < digit || (finalDigit == digit && restAfterCompleteSwitches >powTenIndex)) {
+        totalSwitches += powTenIndex;
+    }
+    
+    return countZeros(m, n,index + 1, zeroCount + totalSwitches);
+}
+
+long countZeros(long m, long n) {
+    return countZeros(m, n, 0, 0);
 }
 
 int main(int argc, const char * argv[])
 {
-    long n;
+    long m, n;
 
-    scanf ("%li", &n);
-    
-    std::vector<long> sequence = getLongestSequence(n);
-    
-    printf("%lu\n", sequence.size());
-    
-    for(long val: sequence) {
-        printf("%li ", val);
+    while(true){
+        scanf("%li %li", &m, &n);
+        if(m < 0){
+            break;
+        }
+        long zeros = countZeros(m, n);
+        printf("%li\n", zeros);
     }
-    printf("\n");
-    
     return 0;
 }
 
